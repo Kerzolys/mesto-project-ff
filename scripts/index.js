18d1ad57b62e595c1c45f3ebd42f26347ce62452
kerzolys <kerzolys@gmail.com>
2024-04-14 09:40:44 +0300

    написаны функции для создания и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-// @todo: Темплейт карточки
+// @todo: Темплейт карточки 
 
 // @todo: DOM узлы
 
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector(".places__list");
+
+let createCardContent = (imgValue, titleValue, deleteFn) => {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener("click", deleteFn);
+
+  cardElement.querySelector(".card__image").src = imgValue;
+  cardElement.querySelector(".card__title").textContent = titleValue;
+
+  return cardElement;
+};
+
+let deleteCard = (evt) => {
+  evt.target.parentNode.remove();
+};
+
+initialCards.forEach((item) => {
+  placesList.append(createCardContent(item.link, item.name, deleteCard));
+});
